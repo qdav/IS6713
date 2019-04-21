dbdebf547d5f4250c1cc20b8f479de740b3f60a0
Quinn <quinnmac@swbell.net>
2019-04-21 15:34:47 -0500

    first draft

--- hw_3/hwcode.py
+++ hw_3/hwcode.py
@@ -4,7 +4,16 @@
 
 import csv
 import numpy as np
+import csv
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer
 # Add any extra imports you want here
+
 
 def classify_tweets_lexicon(csv_filename, positive_lexicon, negative_lexicon):
     '''
@@ -19,7 +28,44 @@ def classify_tweets_lexicon(csv_filename, positive_lexicon, negative_lexicon):
     :return: A float (micro F1 score on test split)
     '''
     # Write code for exercise 1 part 1 here
-    return 0.0
+
+    X = []  # Will be a list
+    y = []  # will be a list
+    with open(csv_filename) as inFile:
+        iCSV = csv.reader(inFile, delimiter=',')
+        next(iCSV)
+        for row in iCSV:
+            X.append(row[5])  # get tweet text
+            y.append(row[0])  # get class
+    X = np.array(X)  # convert to numpy array for scikit-learn
+    y = np.array(y)  # convert to numpy array for scikit-learn
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    predictions = []
+
+    for tweet in X_test:
+        pos_sentiment = 0
+        neg_sentiment = 0
+        overall_sentiment = ''
+
+        for word in tweet.lower().split():
+            if word in (positive_lexicon):
+                pos_sentiment += 1
+            if word in (negative_lexicon):
+                neg_sentiment += 1
+
+        if pos_sentiment >= neg_sentiment:
+            overall_sentiment = "positive"
+        else:
+            overall_sentiment = "negative"
+
+        predictions.append(overall_sentiment)
+
+    y_pred = np.array(predictions)
+    inFile.close()
+    f1_value = f1_score(y_test, y_pred, pos_label='positive')
+
+    return f1_value
 
 def classify_tweets(csv_filename):
     '''
