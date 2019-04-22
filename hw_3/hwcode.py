05e1071e2fd2497e2c7484366a8ba21601f7ecb0
Quinn <quinnmac@swbell.net>
2019-04-21 20:12:39 -0500

    finished first cut at required problems

--- hw_3/hwcode.py
+++ hw_3/hwcode.py
@@ -10,6 +10,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 # Add any extra imports you want here
@@ -63,7 +64,7 @@ def classify_tweets_lexicon(csv_filename, positive_lexicon, negative_lexicon):
 
     y_pred = np.array(predictions)
     inFile.close()
-    f1_value = f1_score(y_test, y_pred, pos_label='positive')
+    f1_value = float(f1_score(y_test, y_pred, pos_label='positive'))
 
     return f1_value
 
@@ -75,9 +76,34 @@ def classify_tweets(csv_filename):
     :return: A list of floats (GridSearchCV micro F1 and micro F1 score on test split)
     '''
     # Write code for exercise 1 part 2 here
-    gridCV_best_score = 0.3
-    test_score = 0.2
-    return [gridCV_best_score, test_score]
+
+    X = []  # Will be a list
+    y = []  # will be a list
+    with open(csv_filename) as inFile:
+        iCSV = csv.reader(inFile, delimiter=',')
+        next(iCSV)
+        for row in iCSV:
+            X.append(row[5])  # get tweet text
+            y.append(row[0])  # get class
+    X = np.array(X)  # convert to numpy array for scikit-learn
+    y = np.array(y)  # convert to numpy array for scikit-learn
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, train_size=0.01, random_state=42)
+
+    vec = CountVectorizer(ngram_range = (1, 1), min_df = 1)
+    cnt_stats = vec.fit(X_train)
+    bow_train = vec.transform(X_train)
+
+    params = {'C': [0.001, 0.01, 0.1, 1., 10.]}
+    clf = GridSearchCV(LinearSVC(), params, cv=3, scoring="f1_micro")
+    clf.fit(bow_train, y_train)
+
+    train_best_score = clf.best_score_
+
+    bow_test = vec.transform(X_test)
+    y_pred = clf.predict(bow_test)
+
+    test_score = float(f1_score(y_test, y_pred, pos_label='positive'))
+    return [train_best_score, test_score]
 
 def predictive_words(csv_filename):
     '''
@@ -88,8 +114,31 @@ def predictive_words(csv_filename):
     :return: A list of strings (words).
     '''
     # Write code for exercise 1 part 2 here
-    top_words = ['best', 'great', 'awesome', 'love', 'good',
-                 'cool', 'happy', 'happily', 'lovely', 'fun']
+
+    X = []  # Will be a list
+    y = []  # will be a list
+    with open(csv_filename) as inFile:
+        iCSV = csv.reader(inFile, delimiter=',')
+        next(iCSV)
+        for row in iCSV:
+            X.append(row[5])  # get tweet text
+            y.append(row[0])  # get class
+    X = np.array(X)  # convert to numpy array for scikit-learn
+    y = np.array(y)  # convert to numpy array for scikit-learn
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, train_size=0.01, random_state=42)
+
+    vec = CountVectorizer(ngram_range = (1, 1), min_df = 1)
+    cnt_stats = vec.fit(X_train)
+    bow_train = vec.transform(X_train)
+
+    # using best c-value from prior problem
+    clf = LinearSVC(C=0.01, random_state=42)
+    clf.fit(bow_train, y_train)
+    coef_list = clf.coef_.transpose().tolist()
+    top_pred_indicies = sorted(range(len(coef_list)), key=lambda i: coef_list[i], reverse=True)[:10]
+
+    top_words = [vec.get_feature_names()[i] for i in top_pred_indicies]
+
     return top_words
 
 
